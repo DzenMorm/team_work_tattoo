b867b2877fc01d0d08300db52fb74fb4b8319236
Den Mormysh <ilodul84@mail.ru>
2021-06-23 13:45:28 +0300

    create templat registration, add form registration, routes separate file

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,22 +1,6 @@
-from flask import Flask, render_template
-from webapp.forms import LoginForm
+from flask import Flask
 
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_pyfile('config.py')
-
-    @app.route('/')
-    def index():
-        title = 'Начальная страница'
-        nav_bar = 'Вход'
-        reg = 'Регистрация'
-        return render_template('index.html', page_title=title, nav_bar_link=nav_bar, reg=reg)
-
-    @app.route('/login')
-    def login():
-        title = "Авторизация"
-        login_form = LoginForm()
-        return render_template('login.html', page_title=title, form=login_form)
-
-    return app
+from webapp import routes
