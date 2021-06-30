fa863f172cb4b1cb24b543877f63be6d96431795
Den Mormysh <ilodul84@mail.ru>
2021-06-30 06:35:47 +0300

    add forms master, user, salon

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,12 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, DateField
 from wtforms.validators import InputRequired, Email, EqualTo
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Электронная почта', validators=[InputRequired()], render_kw={"class": "form-control"})
-    password = PasswordField('Пароль', validators=[InputRequired()], render_kw={"class": "form-control"})
-    submit = SubmitField('Отправить', render_kw={"class": "btn btn-primary"})
+    email = StringField(
+        'Электронная почта',
+        validators=[InputRequired()],
+        render_kw={"class": "form-control"}
+    )
+    password = PasswordField(
+        'Пароль',
+        validators=[InputRequired()],
+        render_kw={"class": "form-control"}
+    )
+    submit = SubmitField(
+        'Отправить',
+        render_kw={"class": "btn btn-primary"}
+    )
 
 
 class RegistrationForm(FlaskForm):
@@ -27,4 +38,69 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField(
         'Зарегистрироваться',
         render_kw={"class": "btn btn-primary"}
+    )
+
+
+class SalonForm(FlaskForm):
+    name = StringField(
+        'Название салона',
+        validators=[InputRequired()]
+    )
+    number_phone = StringField(
+        'Номер телефона',
+        validators=[InputRequired()]
+    )
+    address = StringField(
+        'Адрес',
+        validators=[InputRequired()],
+    )
+    email = StringField(
+        'Электронная почта',
+        validators=[InputRequired(), Email()]
+    )
+
+
+class MasterForm(FlaskForm):
+    name = StringField(
+        'Имя',
+        validators=[InputRequired()]
+    )
+    last_name = StringField(
+        'Фамилия',
+        validators=[InputRequired()]
+    )
+    address = StringField(
+        'Адрес',
+        validators=[InputRequired()],
+    )
+    number_phone = StringField(
+        'Номер телефона',
+        validators=[InputRequired()]
+    )
+    email = StringField(
+        'Электронная почта',
+        validators=[InputRequired(), Email()]
+    )
+
+
+class UserForm(FlaskForm):
+    name = StringField(
+        'Имя',
+        validators=[InputRequired()]
+    )
+    last_name = StringField(
+        'Фамилия',
+        validators=[InputRequired()]
+    )
+    number_phone = StringField(
+        'Номер телефона',
+        validators=[InputRequired()]
+    )
+    email = StringField(
+        'Электронная почта',
+        validators=[InputRequired(), Email()]
+    )
+    date_of_birth = DateField(
+        'Дата рождения',
+        validators=[InputRequired()]
     )
